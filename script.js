0cff6cd597b80dde52ebd80a8cce7ecc3c854c41
Abdelrhman-Abdelmoaty22 <141934349+Abdelrhman-Abdelmoaty22@users.noreply.github.com>
2023-09-27 19:34:03 +0300

    Generate Image AI

---
+++ script.js
@@ -0,0 +1,81 @@
+const apiKey = "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";
+
+const maxImages = 4; 
+let selectedImageNumber = null;
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function disableGenerateButton() {
+    document.getElementById("generate").disabled = true;
+}
+
+function enableGenerateButton() {
+    document.getElementById("generate").disabled = false;
+}
+
+function clearImageGrid() {
+    const imageGrid = document.getElementById("image-grid");
+    imageGrid.innerHTML = "";
+}
+
+async function generateImages(input) {
+    disableGenerateButton();
+    clearImageGrid();
+
+    const loading = document.getElementById("loading");
+    loading.style.display = "block";
+
+    const imageUrls = [];
+
+    for (let i = 0; i < maxImages; i++) {
+        // Generate a random number between 1 and 10000 and append it to the prompt
+        const randomNumber = getRandomNumber(1, 10000);
+        const prompt = `${input} ${randomNumber}`;
+        // We added random number to prompt to create different results
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/prompthero/openjourney",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${apiKey}`,
+                },
+                body: JSON.stringify({ inputs: prompt }),
+            }
+        );
+
+        if (!response.ok) {
+            alert("Failed to generate image!");
+        }
+
+        const blob = await response.blob();
+        const imgUrl = URL.createObjectURL(blob);
+        imageUrls.push(imgUrl);
+
+        const img = document.createElement("img");
+        img.src = imgUrl;
+        img.alt = `art-${i + 1}`;
+        img.onclick = () => downloadImage(imgUrl, i);
+        document.getElementById("image-grid").appendChild(img);
+    }
+
+    loading.style.display = "none";
+    enableGenerateButton();
+
+    selectedImageNumber = null; // Reset selected image number
+}
+
+document.getElementById("generate").addEventListener('click', () => {
+    const input = document.getElementById("user-prompt").value;
+    generateImages(input);
+});
+
+function downloadImage(imgUrl, imageNumber) {
+    const link = document.createElement("a");
+    link.href = imgUrl;
+    // Set filename based on the selected image
+    link.download = `image-${imageNumber + 1}.jpg`;
+    link.click();
+}
